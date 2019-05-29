840c356d677985bb30d1726fb069106fcf67e589
mifsudm <mifsudm@unimelb.edu.au>
2019-05-30 07:48:58 +1000

    ~Auto: Commit

--- src/config/sql/000000.php
+++ src/config/sql/000000.php
@@ -1,23 +1,23 @@
 <?php
 
 $config = \App\Config::getInstance();
-//try {
-//    $data = \Tk\Db\Data::create();
-//
-//    $data->set('site.title', 'TK Base Site');
-//    $data->set('site.short.title', 'tk-base');
-//    $data->set('site.email', 'admin@example.com');
-//    //$data->set('site.client.registration', 'site.client.registration');
-//    //$data->set('site.client.activation', 'site.client.activation');
-//
-//    $data->set('site.meta.keywords', '');
-//    $data->set('site.meta.description', '');
-//    $data->set('site.global.js', '');
-//    $data->set('site.global.css', '');
-//
-//    $data->save();
-//} catch (\Tk\Db\Exception $e) {
-//}
+try {
+    $data = \Tk\Db\Data::create();
+
+    $data->set('site.title', 'Bitcoin Account Client');
+    $data->set('site.short.title', 'BTCC');
+    $data->set('site.email', 'admin@example.com');
+    //$data->set('site.client.registration', 'site.client.registration');
+    //$data->set('site.client.activation', 'site.client.activation');
+
+    $data->set('site.meta.keywords', '');
+    $data->set('site.meta.description', '');
+    $data->set('site.global.js', '');
+    $data->set('site.global.css', '');
+
+    $data->save();
+} catch (\Tk\Db\Exception $e) {
+}
 
 
 
--- src/config/sql/mysql/000000.sql
+++ src/config/sql/mysql/000000.sql
@@ -1,11 +1,20 @@
 
 
-
-
-
 TRUNCATE `user`;
 INSERT INTO `user` (`role_id`, `name`, `email`, `username`, `password`, `hash`, `modified`, `created`)
 VALUES
-  (1, 'Administrator', 'admin@example.com', 'admin', MD5('password'), MD5('1admin'), NOW() , NOW()),
-  (2, 'User', 'user@example.com', 'user1', MD5('password'), MD5('2user'), NOW() , NOW())
+  (1, 'Administrator', 'admin@example.com', 'admin', MD5('password'), MD5('1admin'), NOW() , NOW())
 ;
+
+-- ------------------------------------------------
+--   BTC App DB
+--
+-- ------------------------------------------------
+
+
+
+
+
+
+
+
