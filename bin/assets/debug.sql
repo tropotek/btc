c9e802cb16299938618419e53f47ba53d0804d41
tropotek <info@tropotek.com>
2020-03-20 06:05:56 +1100

    ~Auto: Commit

--- bin/assets/debug.sql
+++ bin/assets/debug.sql
@@ -1,19 +1,20 @@
 -- ------------------------------------------------------
--- EMS II to EMS III migration script Debug SQL
---
+-- Dev setup
 -- Author: Michael Mifsud
--- Date: 06/04/17
+-- Date: 15/03/20
 -- ------------------------------------------------------
-
 
 -- --------------------------------------
 -- Change all passwords to 'password' for debug mode
 -- --------------------------------------
+UPDATE `user` SET `hash` = MD5(CONCAT(`id`, IFNULL(`institution_id`, 0), `username`)) WHERE `hash` = '' OR `hash` IS NULL;
+-- Unsalted
+UPDATE `user` SET `password` = MD5('password') WHERE 1;
+-- UPDATE `user` SET `password` = MD5(CONCAT('password', `hash`)) WHERE 1;
 
--- Salted
--- UPDATE `user` SET `password` = MD5(CONCAT('password', `hash`));
 
--- Unalted
-UPDATE `user` SET `password` = MD5('password');
+
+
+
 
 
---
+++ src/config/sql/mysql/000001.sql
@@ -0,0 +1,18 @@
+-- --------------------------------------------
+-- @version 3.0.0
+--
+-- Author: Michael Mifsud <info@tropotek.com>
+-- --------------------------------------------
+
+
+
+DROP TABLE _user_role;
+DROP TABLE _user_role_id;
+
+
+
+
+
+
+
+
