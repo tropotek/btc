81d3fdf477468ef71e4f31a075172123891007d5
tropotek <info@tropotek.com>
2020-11-22 19:10:03 +1100

    ~Auto: Commit

--- src/config/sql/mysql/000001.sql
+++ src/config/sql/mysql/000001.sql
@@ -13,7 +13,7 @@ rename table migration to _migration;
 */
 
 
-INSERT INTO dev_btc.user (type, username, title, name_first, name_last, email, phone, credentials, position, image, notes, active, last_login, session_id, password, hash, del, modified, created)
+INSERT INTO user (type, username, title, name_first, name_last, email, phone, credentials, position, image, notes, active, last_login, session_id, password, hash, del, modified, created)
 VALUES ('member', 'godar', '', 'Godar', 'Smith', 'info@tropotek.com.au', '0401999999', 'Bsci', 'Web Developer', '', '', 1, null, '', '5f4dcc3b5aa765d61d8327deb882cf99', '766702ce5e24c3ff81f27dbcfc1101a1', 0, NOW(), NOW());
 
 UPDATE exchange t SET t.user_id = 2 WHERE t.id = 1;
