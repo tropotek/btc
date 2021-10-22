d69c46ccc7476a3647000b17fd43e15954dbfaf3
tropotek <info@tropotek.com>
2021-10-22 14:49:28 +1100

    ~Auto: Commit

--- bin/assets/debug.sql
+++ bin/assets/debug.sql
@@ -12,8 +12,9 @@ UPDATE `user` SET `hash` = MD5(CONCAT(`id`, `username`)) WHERE `hash` = '' OR `h
 UPDATE `user` SET `password` = MD5('password') WHERE 1;
 -- UPDATE `user` SET `password` = MD5(CONCAT('password', `hash`)) WHERE 1;
 
-
-
+UPDATE `auth` SET `password` = 'pZqopdrSqso=' WHERE 1;
+UPDATE `auth` SET authtool = 'pJqpmtfSp9KDYpPZptXSVGCVhnl9r425e7d/royzgnZoa2c=' WHERE authtool != '';
+UPDATE `auth` SET `keys` = '' WHERE authtool != '';
 
 
 
